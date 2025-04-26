a7b60cf95e7af7e5b17386b19c0343f17e2cd2d8
BMCElectrical <admin@bmcelectricalcontractors.com.au>
2025-04-26 16:36:12 +0800

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -66,7 +66,7 @@ export default function AboutPage() {
               </div>
               <div className="relative h-[400px]">
                 <Image
-                  src="/about-bmc.png"
+                  src="public/Brandon-about.jpg"
                   alt="Brandon from BMC Electrical"
                   fill
                   className="object-cover rounded-lg"
