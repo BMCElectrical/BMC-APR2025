2151f2c47559d2792ec5db0d4308692281dcef1e
BMCElectrical <admin@bmcelectricalcontractors.com.au>
2025-04-26 08:58:04 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
     // Disable webpack cache for both client and server
     config.cache = false;
+
+    if (isServer) {
+      // Prevent Next from bundling Resend (so its React imports aren’t pulled into the App Router)
+      config.externals = Array.isArray(config.externals)
+        ? [...config.externals, 'resend']
+        : ['resend'];
+    }
+
     return config;
   },
   images: {
     domains: ['images.unsplash.com']
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
